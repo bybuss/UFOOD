93f264fa223e307dbfc1fe0e65803877db6889ae
bybuss <145790087+bybuss@users.noreply.github.com>
2025-02-22 19:06:15 +0300

    Add Spotless & change jvm to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,16 +23,16 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -56,5 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    //implementation(libs.androidx.core.splashscreen)
+    // implementation(libs.androidx.core.splashscreen)
 }
